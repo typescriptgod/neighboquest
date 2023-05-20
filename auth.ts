b4eace01552b9e894255fd20ebbb109c129910a0
typescriptgod <134176643+typescriptgod@users.noreply.github.com>
2023-05-20 19:55:10 -0400

    Add files via upload

--- auth.ts
+++ auth.ts
@@ -1,3 +1,37 @@
-import { writable } from 'svelte/store';
+import jwt from '@fastify/jwt';
+import type { FastifyReply, FastifyRequest } from 'fastify';
+import fp from 'fastify-plugin';
 
-export const user = writable<string | null>(null);
+declare module '@fastify/jwt' {
+	export interface FastifyJWT {
+		payload: {
+			id: string;
+		};
+		user: {
+			id: string;
+		};
+	}
+}
+
+declare module 'fastify' {
+	export interface FastifyInstance {
+		auth: (request: FastifyRequest, reply: FastifyReply) => Promise<void>;
+	}
+}
+
+export default fp(async function (fastify) {
+	await fastify.register(jwt, {
+		secret: process.env.AUTH_SECRET!,
+	});
+
+	fastify.decorate('auth', async function (request: FastifyRequest, reply: FastifyReply) {
+		try {
+			await request.jwtVerify();
+		} catch (err) {
+			reply.send({
+				success: false,
+				message: 'Authentication error.',
+			});
+		}
+	});
+});
